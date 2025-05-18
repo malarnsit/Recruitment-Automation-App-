fa55a1923b16f75c15830f18bf0a3dacca369c6b
malarnsit <159424427+malarnsit@users.noreply.github.com>
2025-05-18 09:47:54 +0530

    Create Candidate.js

---
+++ Candidate.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+
+const candidateSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  applicationStatus: { type: String, required: true },
+});
+module.exports = mongoose.model('Candidate', candidateSchema);
